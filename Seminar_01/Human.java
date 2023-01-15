b28a8dc958d19869d616189041f39c21d2340427
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-15 21:04:02 +0300

    Создан класс Human

--- Seminar_01/Human.java
+++ Seminar_01/Human.java
@@ -2,9 +2,26 @@ package Seminar_01;
 
 public abstract class Human {
     private String name;
+    private int year;
+
+    public Human(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
 
     public Human(String name) {
         this.name = name;
+        this.year = -1;
+    }
+
+    public Human(int year) {
+        this.name = "Неизвестно";
+        this.year = year;
+    }
+
+    public Human() {
+        this.name = "Неизвестно";
+        this.year = -1;
     }
 
     public String getName() {
@@ -15,8 +32,15 @@ public abstract class Human {
         this.name = name;
     }
 
-    @Override
+    public int getBirthday() {
+        return year;
+    }
+
+    public void setBirthday(int year) {
+        this.year = year;
+    }
+
     public String toString() {
-        return name;
+        return name + " " + year;
     }
 }
--- Seminar_01/Main.java
+++ Seminar_01/Main.java
@@ -2,10 +2,5 @@ package Seminar_01;
 
 public class Main {
     public static void main(String[] args) {
-        FamilyTree familyTree = new FamilyTree();
-
-        Link link = new Link("Титов Артём", "Титова Ксения");
-        System.out.println(link);
     }
-    
 }
