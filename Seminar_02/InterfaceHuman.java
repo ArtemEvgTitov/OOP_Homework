50f67009b218667906aab65e3b1b525eb527abe2
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-19 20:56:50 +0300

    Создан интерфейс Human

---
+++ Seminar_02/InterfaceHuman.java
@@ -0,0 +1,13 @@
+package Seminar_02;
+
+/**
+ * Основа для создания Human
+ * Содержит get, set и перевод информации в строку
+ */
+public interface InterfaceHuman {
+    String getName();
+    void setName(String name);
+    int getBirthday();
+    void setBirthday(int year);
+    String humanToString();    
+}
