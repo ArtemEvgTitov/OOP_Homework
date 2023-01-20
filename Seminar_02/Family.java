c5869e76e19118bc553c60760c2dd47c9bd69e70
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-20 14:58:59 +0300

    Готов Family

--- Seminar_02/Family.java
+++ Seminar_02/Family.java
@@ -1,5 +1,52 @@
 package Seminar_02;
 
-public class Family {
-    
+public class Family implements InterfaceHuman {
+    private String name;
+    private int year;
+    private Family prew;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getBirthday() {
+        return year;
+    }
+
+    @Override
+    public void setBirthday(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String humanToString() {
+        return name + " " + year;
+    }
+
+    @Override
+    public void setPrew(Family x) {
+        this.prew = x;
+    }
+
+    @Override
+    public Family getPrew() {
+        return prew;
+    }
+
+    @Override
+    public String prewToString() {
+        if (prew == null) {
+            return "Родственник неизвестен";
+        } else {
+            return prew.humanToString();
+        }
+    }
+
 }
--- Seminar_02/Node.java
+++ Seminar_02/Node.java
@@ -1,5 +1,25 @@
 package Seminar_02;
 
-public class Node {
+import java.util.ArrayList;
+
+public class Node implements InterfaceNode {
+
+    @Override
+    public String printNode() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void preOrder(Node tree, String space) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void children(ArrayList<Node> x) {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
