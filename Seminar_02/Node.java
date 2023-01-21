560eca68a43741620926bf91fa192020bda38616
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-21 21:08:49 +0300

    Готов Node

--- Seminar_02/Node.java
+++ Seminar_02/Node.java
@@ -3,23 +3,41 @@ package Seminar_02;
 import java.util.ArrayList;
 
 public class Node implements InterfaceNode {
+    Family human;
+    ArrayList<Node> son = new ArrayList<Node>();
+
+    public Node(Family human) {
+        this.human = human;
+    }
 
     @Override
     public String printNode() {
-        // TODO Auto-generated method stub
-        return null;
+        return human.humanToString();
     }
 
     @Override
     public void preOrder(Node tree, String space) {
-        // TODO Auto-generated method stub
-        
+        if (tree != null)
+            System.out.println(space + tree.human.humanToString());
+        else {
+            System.out.println(space + "nil");
+            return;
+        }
+
+        for (int i = 0; i < tree.son.size(); i++) {
+            preOrder(tree.son.get(i), space + " * ");
+        }
     }
 
     @Override
     public void children(ArrayList<Node> x) {
-        // TODO Auto-generated method stub
-        
+        if (x.size() == 0) {
+            System.out.println("Отсутствуют");
+        } else {
+            for (int i = 0; i < x.size(); i++) {
+                System.out.println(x.get(i).printNode());
+            }
+        }
     }
     
 }
