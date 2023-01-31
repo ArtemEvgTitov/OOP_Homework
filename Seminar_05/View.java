442c13b81de2b614340d702c9651a93e27bd83f5
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-01-31 22:17:33 +0300

    Создан View

---
+++ Seminar_05/View.java
@@ -0,0 +1,10 @@
+package Seminar_05;
+
+public class View {
+    
+    public Complex getValue(String title){
+        System.out.printf("%s", title);
+        Complex result  = new Complex();
+        return result;
+    }
+}
