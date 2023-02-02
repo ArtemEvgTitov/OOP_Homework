4a2d3ec842cdafc1668a0a48e43edd1ab8354b17
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-02-02 22:01:23 +0300

    Готов SubComplex

--- Seminar_05/Model/ComplexModel/SubComplex.java
+++ Seminar_05/Model/ComplexModel/SubComplex.java
@@ -7,4 +7,11 @@ public class SubComplex extends CalcModel {
     public double result(double x, double y) {
         return x - y;
     }
+
+    public Complex resultComplex(double x1, double x2, double y1, double y2){
+        double newReal = result(x1, x2);
+        double newImage = result(y1, y2);
+		Complex result = new Complex(newReal,newImage);
+		return result;  
+    }
 }
