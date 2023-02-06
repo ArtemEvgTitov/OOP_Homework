08a8faec882850ea22ec230eab8fe4da7d0f79d4
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-02-06 21:49:00 +0300

    ДЗ готово

--- Seminar_06/Main.java
+++ Seminar_06/Main.java
@@ -1,12 +1,13 @@
 package Seminar_06;
 
-import Seminar_05.Console.View;
-import Seminar_05.Model.RationalModel.SumCalc;
-import Seminar_05.Presenter.Presenter;
+import Seminar_06.Console.View;
+import Seminar_06.Model.RationalModel.SumCalc;
+import Seminar_06.Presenter.Presenter;
+
 
 public class Main {
     public static void main(String[] args) {
         Presenter app = new Presenter(new SumCalc(), new View());
-        app.runOperation();
+        app.run();
     }
 }
--- Seminar_06/Presenter/Presenter.java
+++ Seminar_06/Presenter/Presenter.java
@@ -2,16 +2,36 @@ package Seminar_06.Presenter;
 
 import Seminar_06.Console.*;
 import Seminar_06.Model.*;
+import Seminar_06.Presenter.Runner.RunComplex;
+import Seminar_06.Presenter.Runner.RunRatio;
 
 /**
  * Класс с конструктором Presenter
  */
 public class Presenter {
-    ViewModel view;
+    View view;
     CalcModel model;
+    ViewChoiceTypeNumber view3;
 
-    public Presenter(CalcModel m, ViewModel v) {
+    public Presenter(CalcModel m, View view2) {
         this.model = m;
-        this.view = v;
+        this.view = view2;
+    }
+
+    public void run() {
+        int choice = view3.getChoice();
+        switch (choice) {
+            case 1:
+                RunRatio r = new RunRatio();
+                r.run();
+                run();
+            case 2:
+                RunComplex c = new RunComplex();
+                c.run();
+                run();
+            default:
+                run();
+        }
+
     }
 }
