dca4fa3e1307e58f9835163a115e455c1836bcfb
ArtemEvgTitov <artem.titov.wg@gmail.com>
2023-02-10 21:40:15 +0300

    отформатирован код

--- Seminar_06/Console/View.java
+++ Seminar_06/Console/View.java
@@ -3,9 +3,15 @@ package Seminar_06.Console;
 /**
  * Класс View
  */
-public class View {
+public class View extends ViewModel {
 
     public double getValue(String string) {
+        return 0;
+    }
+
+    @Override
+    public int getChoice() {
+        // TODO Auto-generated method stub
         return 0;
     }
 }
